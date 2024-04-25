6e2f21eb84ed7c8c18fddc19375afbdda78c5620
SeniorSherzod <qulmurodovsherzod37@gmail.com>
2024-04-25 14:05:18 +0500

    platform code

--- android/app/src/main/kotlin/com/example/my_default_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_default_project/MainActivity.kt
@@ -1,5 +1,58 @@
 package com.example.my_default_project
 
+import android.os.BatteryManager
+import android.os.Build
+import android.telephony.TelephonyManager
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+
+    private val DEVICE_INFO_CHANNEL = "com.example.device_info_channel"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, DEVICE_INFO_CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getDeviceInfo") {
+                val deviceInfo = getDeviceInfo()
+                result.success(deviceInfo)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun getDeviceInfo(): Map<String, Any?> {
+        val deviceType = getDeviceType()
+        val deviceModel = Build.MODEL
+        val deviceBrand = Build.BRAND
+        val osVersion = Build.VERSION.RELEASE
+        val batteryLevel = getBatteryLevel()
+
+        return mapOf(
+            "deviceType" to deviceType,
+            "deviceModel" to deviceModel,
+            "deviceBrand" to deviceBrand,
+            "osVersion" to osVersion,
+            "batteryLevel" to batteryLevel
+        )
+    }
+
+    private fun getDeviceType(): String {
+        val telephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+        return if (telephonyManager.phoneType != TelephonyManager.PHONE_TYPE_NONE) {
+            "Phone"
+        } else {
+            "Tablet"
+        }
+    }
+
+    private fun getBatteryLevel(): Int {
+        val batteryManager = getSystemService(BATTERY_SERVICE) as BatteryManager
+        return batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+    }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,76 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:my_default_project/screens/demo_screens/demo_screens.dart';
+import 'package:flutter/services.dart';
+import 'package:my_default_project/utils/styles/app_text_style.dart';
 
-import 'data/local/storage_repository.dart';
+void main() {
+  runApp(MyApp());
+}
 
-class MyHttpOverrides extends HttpOverrides {
+class MyApp extends StatelessWidget {
   @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host,
-          int port) => true;
-  }
-
-  Future <void> main() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    StorageRepository();
-    HttpOverrides.global = new MyHttpOverrides();
-    runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(useMaterial3: false),
-        home: DemoScreens(),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Device Info'),
+        ),
+        body: Center(
+          child: DeviceInfoWidget(),
+        ),
       ),
     );
   }
+}
 
+class DeviceInfoWidget extends StatefulWidget {
+  @override
+  _DeviceInfoWidgetState createState() => _DeviceInfoWidgetState();
+}
+
+class _DeviceInfoWidgetState extends State<DeviceInfoWidget> {
+  static const platform = MethodChannel('com.example.device_info_channel');
+
+  String deviceType = 'Unknown';
+  String deviceModel = 'Unknown';
+  String deviceBrand = 'Unknown';
+  String osVersion = 'Unknown';
+  String batteryLevel = 'Unknown';
+
+  @override
+  void initState() {
+    super.initState();
+    _getDeviceInfo();
+  }
+
+  Future<void> _getDeviceInfo() async {
+    try {
+      final dynamic result = await platform.invokeMethod('getDeviceInfo');
+      final Map<String, dynamic> deviceInfo = Map<String, dynamic>.from(result);
+      setState(() {
+        deviceType = deviceInfo['deviceType'] ?? 'Unknown';
+        deviceModel = deviceInfo['deviceModel'] ?? 'Unknown';
+        deviceBrand = deviceInfo['deviceBrand'] ?? 'Unknown';
+        osVersion = deviceInfo['osVersion'] ?? 'Unknown';
+        batteryLevel = deviceInfo['batteryLevel']?.toString() ?? 'Unknown'; // Convert to string
+      });
+    } on PlatformException catch (e) {
+      print("Failed to get device info: '${e.message}'.");
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text("Sherzod Qulmurodov",style: AppTextStyle.GilroyMedium.copyWith(fontSize: 30),),
+        Text('Device Type: $deviceType'),
+        Text('Device Model: $deviceModel'),
+        Text('Device Brand: $deviceBrand'),
+        Text('OS Version: $osVersion'),
+        Text('Battery Level: $batteryLevel'),
+      ],
+    );
+  }
 }
